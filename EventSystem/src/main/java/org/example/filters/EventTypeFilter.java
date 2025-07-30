90e27d65c9f81f0eeb5a66f4ecfb6273d137f8e6
Kareem Al-Qutob <97916953+Kareem-izzat@users.noreply.github.com>
2025-07-30 09:19:43 +0300

    enhanced filtering and added filtered subscriber

--- EventSystem/src/main/java/org/example/filters/EventTypeFilter.java
+++ EventSystem/src/main/java/org/example/filters/EventTypeFilter.java
@@ -2,7 +2,7 @@ package org.example.filters;
 
 import org.example.events.EventType;
 import org.example.events.Event;
-
+// filter based on filter type
 public class EventTypeFilter<T extends Event> implements EventFilter<T> {
     private final EventType eventType;
 
